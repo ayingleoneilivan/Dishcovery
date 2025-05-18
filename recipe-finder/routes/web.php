a3043ea85ccb56b83b0251361dc24702db807d91
unknown <vtiu635@gmail.com>
2025-05-18 09:29:37 +0800

    added new routes

--- recipe-finder/routes/web.php
+++ recipe-finder/routes/web.php
@@ -3,18 +3,26 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-
-Route::get('/', function () {
-    return view('homepage');
-});
-
+// Existing Routes
 Route::get('/product-details', function () {
     return view('product_details');
 })->name('product_details');
 
-Route::get('/', [MealController::class, 'index'])->name('meals.index');
+Route::get('/', function () {
+    return view('auth.login');
+});
+
 Route::get('/meals', [MealController::class, 'index'])->name('meals.index');
 Route::get('/test-api', [MealController::class, 'testApi']);
+Route::post('/favorite/toggle', [FavoriteController::class, 'toggle'])->name('favorite.toggle')->middleware('auth');
+Route::get('/favorites', [FavoriteController::class, 'index'])
+    ->name('favorites.index')
+    ->middleware('auth');
+Route::get('/meal/{id}', [MealController::class, 'show'])->name('meal.details');
+//temporary
+Route::get('/meals/search', [MealController::class, 'search'])->name('meals.search');
 
 require __DIR__.'/auth.php';
